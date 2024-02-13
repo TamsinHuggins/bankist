9ad4278e9d67847d94ec95828f76b12c1a724833
Tamsin Huggins <tamsin.hns@gmail.com>
2024-02-13 16:32:02 +0000

    new function to compute username from initials in full name

--- script.js
+++ script.js
@@ -61,11 +61,15 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-// log in
-
+/**
+ * Displays the movements of money in the UI.
+ *
+ * @param {Array<number>} movements - The array of financial transactions to be displayed.
+ */
 const displayMovements = function (movements) {
+  // clear the container
   containerMovements.innerHTML = '';
-
+  // for each movement, create a new div and insert it into the container
   const displayOne = function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `<div class="movements__row">
@@ -81,6 +85,20 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  // for every account holder in the accounts array, compute the username attribute from the full name of the account owner
+
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -93,18 +111,34 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// loop over movements to display message for each
+// --- USING MAP /////
 
-const moveMoney = function (movement, index) {
-  const msg =
-    movement > 0
-      ? `deposit of ${movement}`
-      : `you withdrew ${Math.abs(movement)}`;
-  console.log(`Transaction ${index}:` + msg);
-};
+const movementDescriptions = movements.map(
+  (transact, index) =>
+    `You ${transact > 0 ? 'deposited' : 'withdrew'} ${transact}`
+);
 
-//forEach loops over and calls the function once for every element in the array, passing in the element as that function's one arg.
-// if you give the callback func 2 args then the forEach function passes the current index as the second arg.
-movements.forEach(moveMoney);
+console.log(movementDescriptions);
+
+//  ----- USING FOR EACH ---------------------
+
+// const moveMoney = function (movement, index) {
+//   const msg =
+//     movement > 0
+//       ? `deposit of ${movement}`
+//       : `you withdrew ${Math.abs(movement)}`;
+//   console.log(`Transaction ${index}:` + msg);
+// };
+
+// //forEach loops over and calls the function once for every element in the array, passing in the element as that function's one arg.
+// // if you give the callback func 2 args then the forEach function passes the current index as the second arg.
+// movements.forEach(moveMoney);
 
 /////////////////////////////////////////////////
+
+const eurToUSD = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUSD);
+
+console.log(movements);
+console.log(movementsUSD);
